608ab0dbb3f2a841fa5ffd375f02731ece51d120
Krishna Vishwakarma <krishnavishwakarma2525@gmail.com>
2023-12-04 21:12:39 +0530

    changed

--- lib/res/strings/const.dart
+++ lib/res/strings/const.dart
@@ -1,3 +1,3 @@
-String aPIKEY = "APIKEY";
+String aPIKEY = "AIzaSyADVXAOtuGutYX2Td23tEjmkP8v-3Rx4SI";
 String baseUrl =
     "https://generativelanguage.googleapis.com/v1beta3/models/text-bison-001:generateText?key=$aPIKEY";
